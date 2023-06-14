f4a1224aa8c38efe165796574d6c3ef64d78d95e
Thippeswamy A H <M276977@ONE.merckgroup.com>
2023-06-14 18:18:35 +0530

    added test APi

--- demo/src/main/java/com/example/demo/TestController.java
+++ demo/src/main/java/com/example/demo/TestController.java
@@ -1,6 +1,8 @@
 package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,9 +14,15 @@ public class TestController {
 
 	@Autowired
 	RestTemplate restTemplate;
-	
+
 	@GetMapping("/resttemplate")
-	public  void test(){
+	public void test() {
 		System.out.println(restTemplate);
 	}
+
+	@GetMapping("/docker/container")
+	public ResponseEntity<String> test1() {
+		return new ResponseEntity<String>("Hello from docker container test API", HttpStatus.OK);
+	}
+
 }
